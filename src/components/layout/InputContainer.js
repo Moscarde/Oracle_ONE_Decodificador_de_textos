15080e20ed0d895c4de0b49f85e103054c6dc5e7
Moscarde <theizera@gmail.com>
2023-04-20 23:17:36 -0300

    att input output

--- src/components/layout/InputContainer.js
+++ src/components/layout/InputContainer.js
@@ -1,17 +1,52 @@
+import React, { useState } from 'react';
+
+// styles
 import styles from './InputContainer.module.css'
+import AttentionImg from '../../assets/img/Attention.svg'
 
 function InputContainer() {
+    const [mode, setMode] = useState('encrypt');
+    const [text, setText] = useState('')
+
+    // const handleClick = (mode) => {
+    //     setIsActive(mode);
+    // };
+
+    let activeOption = 'decrypt'
+
+    function changeMode(mode) {
+        setMode(mode)
+    }
+
+    function handleChange(e) {
+        setText(e.target.value)
+        console.log(text)
+    }
+
+
     return (
         <div className={styles.container}>
-            <textarea name="inputText" placeholder="Digite seu texto" className={styles.text_area} ></textarea>
-            <p>Apenas letras minusculas e sem acento</p>
-            <div className={styles.actions}>
-                <button>Criptografar</button>
-                <button>Descriptografar</button>
-                
-                <button></button>
+            <textarea name="inputText" placeholder="Digite seu texto" className={styles.text_area} onChange={handleChange} />
+            <div className={styles.description}>
+                <div
+                    style={{
+                        backgroundImage: `url(${AttentionImg})`
+                    }}
+                    className={styles.attention_icon}>
+                </div>
+                <p>Apenas letras minusculas e sem acento</p>
             </div>
-        </div>
+            <div className={styles.actions}>
+                <button
+                    onClick={() => {changeMode('encrypt')}}
+                    className={`${styles.button} ${mode === 'encrypt' ? `${styles.active}` : ''}`}>
+                    Criptografar
+                </button>
+                <button
+                    onClick={() => {changeMode('decrypt')}}
+                    className={`${styles.button} ${mode === 'decrypt' ? `${styles.active}` : ''}`}>Descriptografar</button>
+            </div>
+        </div >
     )
 }
 
--- src/components/layout/OutputContainer.js
+++ src/components/layout/OutputContainer.js
@@ -1,9 +1,20 @@
 import styles from './OutputContainer.module.css'
 
+import emptyTextImg from '../../assets/img/EmptText.svg'
+
 function OutputContainer() {
     return (
         <div className={styles.container}>
-            <p>Output Container</p>
+
+            <div
+                style={{
+                    backgroundImage: `url(${emptyTextImg})`
+                }}
+                className={styles.empty_text_img}>
+                
+            </div>
+            <h2>Nenhuma mensagem encontrada</h2>
+            <p>Digite um texto que você deseja criptografar ou descriptografar.</p>
         </div>
     )
 }
